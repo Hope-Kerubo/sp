a66dbb2e1b010cb0847bf052328d9774bfd040b9
Hope-Kerubo <kerubohopes@mail.com>
2023-11-15 22:09:07 +0300

    comments

--- socketServer.py
+++ socketServer.py
@@ -16,7 +16,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             conn.sendall(data)
 
 """
-with- used to wrap the execution of a block with methods defined by a context manager 
-socket() - returns a socket object whose methods implement the various socket system calls
+with- used to wrap the execution of a block with methods defined by a context manager.
+socket() - returns a socket object whose methods implement the various socket system calls.
+aruments passed in socket() are constants used to specify the address family and socket type.
+AF_INET is the Internet address family for IPv4. 
+SOCK_STREAM is the socket type for TCP, the protocol that will be used to transport messages in the network.
+values passed to .bind depend on the address family of the socket. 
+In this case, were using ipv4 so it expects to records, a host and a port.
 
 """
